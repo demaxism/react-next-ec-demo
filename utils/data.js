13a4f14bc7427582d44f49936a185d299da1d25c
ding <ding@ding>
2022-07-21 12:48:17 +0900

    update

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'John',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: 'Free Shirt',
@@ -76,7 +91,6 @@ const data = {
       countInStock: 20,
       description: 'A popular pants',
     },
-  ]
+  ],
 };
-
 export default data;
